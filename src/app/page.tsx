ffb06f14cc40b828a4d26c46e0c629bb5972443a
RadekDev <radekdev@Radoslaws-MacBook-Pro.local>
2023-10-18 20:37:24 +0200

    add prettierrc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,26 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home({ children }: { children: React.ReactNode }) {
-	return (
-		<>
-			<header>
-				<nav>
-					<ul>
-						<li>
-							<Link href="/">About</Link>
-						</li>
-						<li>
-							<Link href="/">Posts</Link>
-						</li>
-					</ul>
-				</nav>
-			</header>
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/">Posts</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
 
-			<main>{children}</main>
+            <main>{children}</main>
 
-			<footer>
-				<p>Footer</p>
-			</footer>
-		</>
-	);
+            <footer>
+                <p>Footer</p>
+            </footer>
+        </>
+    );
 }
