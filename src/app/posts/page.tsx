9ee5ea57f45912e62627cea721b33c2a8d863961
RadekDev <radekdev@Radoslaws-MacBook-Pro.local>
2023-10-18 23:28:22 +0200

    install contentful & get content

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,13 +2,34 @@ import Link from "next/link";
 import Footer from "../components/footer";
 import Header from "../components/header";
 
-export default function Posts() {
+import styles from "./posts.module.css";
+
+import { client } from "../lib/contentful/client";
+
+export default async function Posts() {
+    const response = await client.getEntries({ content_type: "post" });
+
+    console.log("test: ", response.items[0]);
+    const postItems = response.items;
     return (
         <>
             <Header />
             <Link href="/">Home</Link>
             <h1>Posts</h1>
+            {postItems.map((item: any) => {
+                return (
+                    <div className={styles.post} key={item.sys.id}>
+                        <p>created at: {item.sys.createdAt}</p>
+                        <p>{item.fields.title}</p>
+                        <p>{item.fields.body}</p>
+                    </div>
+                );
+            })}
             <Footer />
         </>
     );
 }
+
+// export const getStaticProps = async () => {
+
+// };
