e19bceedd1eff7c89a287f1b488a997672661bb1
RadekDev <radekdev@Radoslaws-MacBook-Pro.local>
2023-10-18 20:22:27 +0200

    add Links & Footer to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ children }: { children: React.ReactNode }) {
 	return (
-		<main className={styles.main}>
-			<p>Hallo</p>
-		</main>
+		<>
+			<header>
+				<nav>
+					<ul>
+						<li>
+							<Link href="/">About</Link>
+						</li>
+						<li>
+							<Link href="/">Posts</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
+			<main>{children}</main>
+
+			<footer>
+				<p>Footer</p>
+			</footer>
+		</>
 	);
 }
