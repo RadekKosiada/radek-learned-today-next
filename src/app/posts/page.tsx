aad5508b764e73288e5a1d3c0349cb2f16ace5bd
RadekDev <radekdev@Radoslaws-MacBook-Pro.local>
2023-10-21 11:05:44 +0200

    rename postsArray

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -13,28 +13,28 @@ export default async function Posts() {
     const response = await client.getEntries({ content_type: "post" });
 
     console.log("test: ", response.items[0].fields.body);
-    const postItems = response.items;
+    const { items: postsArray } = response;
 
     return (
         <>
             <Header />
             <Link href="/">Home</Link>
             <h1>Posts</h1>
-            {postItems.map((item: any) => {
+            {postsArray.map((post: "post") => {
                 return (
-                    <div className={styles.post} key={item.sys.id}>
-                        <h3>{item.fields.title}</h3>
+                    <div className={styles.post} key={post.sys.id}>
+                        <h3>{post.fields.title}</h3>
                         <Image
-                            src={`https:${item.fields.category.fields.icon.fields.file.url}`}
+                            src={`https:${post.fields.category.fields.icon.fields.file.url}`}
                             alt={"abc"}
                             width={100}
                             height={100}
                         />
                         {/* <p>created at: {item.sys.createdAt}</p> */}
-                        <p>published at: {item.fields.publishDate}</p>
-                        <p>category: {item.fields.category.fields.title}</p>
-                        <ReactMarkdown>{item.fields.body}</ReactMarkdown>
-                        <p>slug: {item.fields.slug}</p>
+                        <p>published at: {post.fields.publishDate}</p>
+                        <p>category: {post.fields.category.fields.title}</p>
+                        <ReactMarkdown>{post.fields.body}</ReactMarkdown>
+                        <p>slug: {post.fields.slug}</p>
                     </div>
                 );
             })}
