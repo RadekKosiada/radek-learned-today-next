fdbff053dc1a7d046c0055116207650127d7eafb
RadekDev <radekdev@Radoslaws-MacBook-Pro.local>
2023-10-22 10:14:49 +0200

    move post's data to slug page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,13 +1,45 @@
 import Link from "next/link";
+import ReactMarkdown from "react-markdown";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import { client } from "../lib/contentful/client";
+import Image from "next/image";
 
-export default function Slug() {
+export default async function Slug({ params }: { params: { slug: string } }) {
+    const { slug } = params;
+    const response = await client.getEntries({
+        content_type: "post",
+        "fields.slug": slug,
+    });
+
+    const { items } = response;
+
+    const {
+        fields: { title, category, publishDate, body },
+    } = items[0];
+
     return (
         <>
             <Header />
             <Link href="/">Home</Link>
-            <h1>Slug</h1>
+
+            <h3>{title}</h3>
+            {category?.fields.icon?.fields.file && (
+                <Image
+                    src={`https:${category.fields.icon.fields.file.url}`}
+                    alt={"abc"}
+                    width={100}
+                    height={100}
+                />
+            )}
+
+            {/* <p>created at: {item.sys.createdAt}</p> */}
+            <p>published at: {publishDate}</p>
+            {category && <p>category: {category.fields.title}</p>}
+
+            <ReactMarkdown>{body}</ReactMarkdown>
+            <p>slug: {slug}</p>
+
             <Footer />
         </>
     );
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import ReactMarkdown from "react-markdown";
 import Footer from "../components/footer";
 import Header from "../components/header";
 
@@ -7,7 +6,6 @@ import styles from "./posts.module.css";
 
 import { client } from "../lib/contentful/client";
 
-import Image from "next/image";
 import { TypePost } from "../../../types/contentful";
 
 export default async function Posts() {
@@ -23,29 +21,14 @@ export default async function Posts() {
             {postsArray.map((post: TypePost) => {
                 const {
                     sys,
-                    fields: { title, category, publishDate, body, slug },
+                    fields: { title, category, slug },
                 } = post || {};
 
                 return (
                     <div className={styles.post} key={sys.id}>
+                        <Link href={`/${slug}`}>HERE</Link>
                         <h3>{title}</h3>
-                        {category?.fields.icon?.fields.file && (
-                            <Image
-                                src={`https:${category.fields.icon.fields.file.url}`}
-                                alt={"abc"}
-                                width={100}
-                                height={100}
-                            />
-                        )}
-
-                        {/* <p>created at: {item.sys.createdAt}</p> */}
-                        <p>published at: {publishDate}</p>
-                        {category && (
-                            <p>category: {category.fields.title}</p>
-                        )}
-
-                        <ReactMarkdown>{body}</ReactMarkdown>
-                        <p>slug: {slug}</p>
+                        {category && <p>category: {category.fields.title}</p>}
                     </div>
                 );
             })}
