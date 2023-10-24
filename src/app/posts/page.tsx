aaabaadda7a4400e78427659a580c833d1cd8bb8
RadekDev <radekdev@Radoslaws-MacBook-Pro.local>
2023-10-24 17:19:57 +0200

    set grid

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -8,7 +8,6 @@ import { TypePost } from "../../../types/contentful";
 import { responseType } from "../../../types/contentful/TypePost";
 
 // https://nextjs.org/docs/app/building-your-application/caching#request-memoization
-
 export async function getPosts() {
     const response: responseType = await client.getEntries({
         content_type: "post",
@@ -24,21 +23,27 @@ export default async function Posts() {
     return (
         <>
             <h1>Posts</h1>
-            {postsArray.map((post: TypePost) => {
-                const {
-                    sys,
-                    fields: { title, category, slug },
-                } = post || {};
+            <div className={styles.postContainer}>
+                <div className={styles.postsWrapper}>
+                    {postsArray.map((post: TypePost) => {
+                        const {
+                            sys,
+                            fields: { title, category, slug },
+                        } = post || {};
 
-                return (
-                    <div className={styles.post} key={sys.id}>
-                        <Link href={`/${slug}`}>
-                            <h3>{title}</h3>
-                        </Link>
-                        {category && <p>category: {category.fields.title}</p>}
-                    </div>
-                );
-            })}
+                        return (
+                            <div className={styles.post} key={sys.id}>
+                                <Link href={`/${slug}`}>
+                                    <h3>{title}</h3>
+                                </Link>
+                                {category && (
+                                    <p>category: {category.fields.title}</p>
+                                )}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </>
     );
 }
