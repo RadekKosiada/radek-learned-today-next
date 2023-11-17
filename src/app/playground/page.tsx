103a13101ebe3ddd44d414418be8c403a55c939a
RadekKosiada <radekkosiada@Radoslaws-MacBook-Pro.local>
2023-11-17 17:29:43 +0100

    add sass

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import styles from "./playground.module.css";
+import styles from "./playground.module.scss";
 
 function ExampleWrapper({ child }: { child: ReactNode }) {
     return <div className={styles.examplesWrapper}>{child}</div>;
