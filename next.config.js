375a4a9777e4537703f95b8027ffa57a0253df26
RadekKosiada <radekkosiada@Radoslaws-MacBook-Pro.local>
2023-11-17 18:10:38 +0100

    adjust nextConfig w/sassOptions

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
     images: {
         domains: ["images.ctfassets.net"],
     },
-    // sassOptions: {
-    //     includePaths: [path.join(__dirname, "styles")],
-    // },
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+    },
 };
 
 module.exports = nextConfig;
