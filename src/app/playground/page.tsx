0a0599a24e4eeab7cbf5c418f02cc77b33a3b0c1
RadekKosiada <radekkosiada@Radoslaws-MacBook-Pro.local>
2023-11-20 12:52:29 +0100

    adjust padding and grid in Playground

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -4,7 +4,7 @@ import styles from "./playground.module.scss";
 import Header from "../components/header";
 
 function ExampleWrapper({ child }: { child: ReactNode }) {
-    return <div className={styles.examplesWrapper}>{child}</div>;
+    return <div className={styles.exampleWrapper}>{child}</div>;
 }
 
 export default function Playground() {
@@ -12,20 +12,24 @@ export default function Playground() {
     return (
         <>
             <Header />
-            <h1>Playground...</h1>
-            <ExampleWrapper
-                child={
-                    <>
-                        <p>Example 1</p>
-                        <h2
-                            data-text={textExample1}
-                            className={styles.example1Text}
-                        >
-                            {textExample1}
-                        </h2>
-                    </>
-                }
-            />
+            <div className={styles.wrapper}>
+                <h1>Playground...</h1>
+                <div className={styles.gridWrapper}>
+                    <ExampleWrapper
+                        child={
+                            <>
+                                <p>Example 1</p>
+                                <h2
+                                    data-text={textExample1}
+                                    className={styles.example1Text}
+                                >
+                                    {textExample1}
+                                </h2>
+                            </>
+                        }
+                    />
+                </div>
+            </div>
 
             <Footer />
         </>
