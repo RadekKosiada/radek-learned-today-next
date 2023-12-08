814ee40750559c2b456a3fbc542a92e10db31d79
RadekKosiada <radekkosiada@bm1055.bmeister.biz>
2023-12-08 15:27:27 +0100

    add burger menu for screens <= 992px

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,24 +3,42 @@
 import Link from "next/link";
 import styles from "./header.module.scss";
 import { usePathname } from "next/navigation";
+import { MouseEvent, useEffect, useState } from "react";
 
 export default function Header() {
     const pathname = usePathname();
+    const [burgerIsChecked, setBurgerIsChecked] = useState(false);
+
+    const handleCheck = (event: MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setBurgerIsChecked(!burgerIsChecked);
+    };
+
+    const handleResize = () => {
+        if (window.innerWidth >= 992) {
+            setBurgerIsChecked(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     const homePathname = "/";
     const playgroundPathname = "/playground";
     const aboutPathname = "/about";
 
-    const isPageFromHeader = [
-        homePathname,
-        playgroundPathname,
-        aboutPathname,
-    ].includes(pathname);
-
     return (
         <header className={styles.headerWrapper}>
             <nav>
-                <ul className={!isPageFromHeader ? styles.tripleColumn : ""}>
+                <ul
+                    className={
+                        burgerIsChecked ? styles.vertical : styles.horizontal
+                    }
+                >
                     {pathname !== homePathname && (
                         <li key="posts">
                             <Link href={homePathname}>Back to Posts</Link>
@@ -37,6 +55,21 @@ export default function Header() {
                         </li>
                     )}
                 </ul>
+                {/* display: none set in SCSS if screen > 992px */}
+                <div className={styles.checkboxWrapper} onClick={handleCheck}>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={burgerIsChecked}
+                            readOnly
+                        />
+                        <div
+                            className={styles.menuButton}
+                            role="checkbox"
+                            aria-checked={burgerIsChecked}
+                        ></div>
+                    </label>
+                </div>
             </nav>
         </header>
     );
