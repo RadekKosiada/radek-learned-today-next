b7f2b36335195cfb879fbd96a655dc334ea0235e
RadekKosiada <kosiadar@gmail.com>
2025-11-08 14:42:38 +0100

    fix destructuring in about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,17 @@
 import AboutComponent from "../components/aboutComponent";
 import Header from "../components/header";
 
-import styles from "./about.module.scss";
+import { TypeAboutMeFields } from "../../../types/contentful/TypeAboutMe";
 import getAboutMe from "../../api/getAboutMe";
+import styles from "./about.module.scss";
 
 export default async function AboutPage() {
     const responseAbout = await getAboutMe();
 
     const { items } = responseAbout;
 
-    const {
-        fields: { body: aboutText },
-    } = items[0];
+    const fields = items[0]?.fields as TypeAboutMeFields | undefined;
+    const aboutText = fields?.body ?? "";
 
     return (
         <>
