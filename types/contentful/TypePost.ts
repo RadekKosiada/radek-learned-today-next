7008dfa4e8b4bd0e329e2702630efc49edc4d357
RadekKosiada <kosiadar@gmail.com>
2025-11-08 12:12:55 +0100

    adjust types

--- types/contentful/TypePost.ts
+++ types/contentful/TypePost.ts
@@ -1,10 +1,12 @@
 import type { Entry, EntryFields } from "contentful";
 import type { TypeCategoryFields } from "./TypeCategory";
+import type { TypePostTypeFields } from "./TypePostType";
 
 export interface TypePostFields {
     title?: EntryFields.Symbol;
     slug?: EntryFields.Symbol;
     publishDate: EntryFields.Date;
+    type?: Entry<TypePostTypeFields>[];
     body?: EntryFields.Text;
     category: Entry<TypeCategoryFields>;
     postRichText?: EntryFields.RichText;
